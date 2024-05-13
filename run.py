4fe75a5b0c5c6c0a00b67777910402b3585edc12
DRAGONSTONE\Chathuranga <csampath.personal@gmail.com>
2024-05-14 02:00:30 +0530

    feat: enable command-line args for single/multiple switches
    Change made:
    - command-line supports [ set / get ] status
    - add a dummy device_map.json for reference

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from tinytuya import OutletDevice
 
@@ -14,8 +15,29 @@ def findDeviceByName(name):
                 )
         return None
 
-d = findDeviceByName("Bedroom lights")
-if d:
-    d.set_status(False, 1)
-else:
-    print("Device not found.")
+def main():
+    args = sys.argv[1:]  # Exclude the script name
+    if not args:
+        print("Please provide arguments. Usage: python run.py <name> [<status1> <status2> <status3> <status4>]")
+        return
+
+    name = args[0]
+    device = findDeviceByName(name)
+    if device:
+        if len(args) == 1:
+            data = device.status()
+            print(data)
+        else:
+            statuses = [arg.lower() == 'true' for arg in args[1:]]
+            if len(statuses) == 1:
+                device.set_status(statuses[0])
+            else:
+                for i, status in enumerate(statuses, start=1):
+                    if i > 4:
+                        break
+                    device.set_status(status, i)
+    else:
+        print("Device not found.")
+
+if __name__ == "__main__":
+    main()
