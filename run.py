d2d06fc7375ddc2f1443d8d0fc8399c0b4083e68
DRAGONSTONE\Chathuranga <csampath.personal@gmail.com>
2024-05-15 20:27:31 +0530

    feat: Add timeouts for command executions

--- run.py
+++ run.py
@@ -1,10 +1,24 @@
 import sys
 import json
 import argparse
+import os
+import concurrent.futures
 from tinytuya import OutletDevice
 
+TIMEOUT = 4  # Timeout in seconds
+
 def findDevice(identifier, search_by='name'):
-    with open('device_map.json', 'r') as file:
+    # Determine the path to the JSON file relative to the executable
+    if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the path will be relative to the executable
+        script_dir = os.path.dirname(sys.executable)
+    else:
+        # If run in a normal Python environment, the path will be relative to the script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    json_path = os.path.join(script_dir, 'device_map.json')
+    
+    with open(json_path, 'r') as file:
         data = json.load(file)
         for device in data:
             if search_by == 'name' and device['name'] == identifier:
@@ -23,6 +37,16 @@ def findDevice(identifier, search_by='name'):
                 )
         return None
 
+def execute_with_timeout(function, *args, **kwargs):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(function, *args, **kwargs)
+        try:
+            result = future.result(timeout=TIMEOUT)
+            return result
+        except concurrent.futures.TimeoutError:
+            print(f"Error: Operation timed out after {TIMEOUT} seconds")
+            sys.exit(1)
+
 def main():
     parser = argparse.ArgumentParser(description="Control smart devices by name or ID.")
     group = parser.add_mutually_exclusive_group(required=True)
@@ -40,14 +64,14 @@ def main():
     if device:
         if not args.statuses:
             # Get Status
-            data = device.status()
-            print('get_status() result:', data)
+            data = execute_with_timeout(device.status)
+            print(data)
         else:
             for i, arg in enumerate(args.statuses, start=1):
                 if arg.lower() == 'null':
                     continue  # Skip null values
                 status = arg.lower() == 'true'
-                device.set_status(status, i)
+                execute_with_timeout(device.set_status, status, i)
     else:
         print("Device not found.")
 
